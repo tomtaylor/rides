0b38fba00e4596933e97cea3c823c0fa29a79a5d
Tom Taylor <tom@tomtaylor.co.uk>
2012-12-31 15:09:23 +0000

    retina tiles

--- source/javascripts/map.js
+++ source/javascripts/map.js
@@ -8,7 +8,7 @@ window.MapManager = function (id) {
 window.MapManager.prototype.bootstrap = function () {
   this.map = new L.Map(this.id, { scrollWheelZoom: false, attributionControl: false })
       .setView(new L.LatLng(51.5073, 0.1276), 8)
-      .addLayer(new L.TileLayer("http://{s}.tiles.mapbox.com/v3/examples.map-4l7djmvo/{z}/{x}/{y}.png"));
+      .addLayer(new L.TileLayer("http://{s}.tiles.mapbox.com/v3/examples.map-4l7djmvo/{z}/{x}/{y}.png", { detectRetina: true }));
 };
 
 window.MapManager.prototype.addRoute = function (route) {
