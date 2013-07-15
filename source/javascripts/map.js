74caaf058686d5ec83f04b087f483678c1a99628
Tom Taylor <tom@tomtaylor.co.uk>
2013-07-15 15:17:56 +0100

    Update map URL.

--- source/javascripts/map.js
+++ source/javascripts/map.js
@@ -8,7 +8,7 @@ window.MapManager = function (id) {
 window.MapManager.prototype.bootstrap = function () {
   this.map = new L.Map(this.id, { scrollWheelZoom: false, attributionControl: false })
       .setView(new L.LatLng(51.5073, 0.1276), 8)
-      .addLayer(new L.TileLayer("http://{s}.tiles.mapbox.com/v3/examples.map-4l7djmvo/{z}/{x}/{y}.png", { detectRetina: true }));
+      .addLayer(new L.TileLayer("http://{s}.tiles.mapbox.com/v3/tomtaylor.map-q0kbynb9/{z}/{x}/{y}.png", { detectRetina: true }));
 };
 
 window.MapManager.prototype.addRoute = function (route) {
